f5c5c7e4d9f9b56a9dfb9e26c5a6c55eea9228e4
Alexandre Angrignon <alex040892@gmail.com>
2024-11-19 15:18:11 +1300

    Hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import logo from './logo.svg';
 import Navbar from './Components/Navbar/Navbar';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import Product from './Pages/Product';
+import ShopCategory from './Pages/ShopCategory';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
 function App() {
   return (
-    <div className=''>
-      <Navbar />
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/mens' element={<ShopCategory category='men' />} />
+          <Route path='/womens' element={<ShopCategory category='womens' />} />
+          <Route path='/kids' element={<ShopCategory category='kids' />} />
+          <Route path='/product' element={<Product />}>
+            <Route path=':productId' element={<Product />} />
+          </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
