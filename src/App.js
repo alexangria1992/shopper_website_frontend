a0849f09b0b9eba37ef217a072b85aeb2a718fd9
Alexandre Angrignon <alex040892@gmail.com>
2024-11-20 13:32:21 +1300

    Context & ShopCategory

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Shop from './Pages/Shop';
@@ -7,6 +6,10 @@ import ShopCategory from './Pages/ShopCategory';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png';
+
 function App() {
   return (
     <div>
@@ -14,9 +17,18 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/mens' element={<ShopCategory category='men' />} />
-          <Route path='/womens' element={<ShopCategory category='womens' />} />
-          <Route path='/kids' element={<ShopCategory category='kids' />} />
+          <Route
+            path='/mens'
+            element={<ShopCategory banner={men_banner} category='men' />}
+          />
+          <Route
+            path='/womens'
+            element={<ShopCategory banner={women_banner} category='women' />}
+          />
+          <Route
+            path='/kids'
+            element={<ShopCategory banner={kid_banner} category='kid' />}
+          />
           <Route path='/product' element={<Product />}>
             <Route path=':productId' element={<Product />} />
           </Route>
