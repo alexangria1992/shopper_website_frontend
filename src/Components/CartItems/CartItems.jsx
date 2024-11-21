be781cbd72641081664cb4f57553560d9fe95c7f
Alexandre Angrignon <alex040892@gmail.com>
2024-11-21 15:46:37 +1300

    Total Amount and Total Items functions

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -3,7 +3,8 @@ import './CartItems.css';
 import { ShopContext } from '../../Context/ShopContext';
 import remove_icon from '../Assets/cart_cross_icon.png';
 const CartItems = () => {
-  const { all_product, cartItems, removeFromCart } = useContext(ShopContext);
+  const { getTotalCartAmount, all_product, cartItems, removeFromCart } =
+    useContext(ShopContext);
   return (
     <div className='cartitems'>
       <div className='cartitems-format-main'>
@@ -43,7 +44,7 @@ const CartItems = () => {
           <div>
             <div className='cartitems-total-item'>
               <p>Subtotal</p>
-              <p>${0}</p>
+              <p>${getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className='cartitems-total-item'>
@@ -53,7 +54,7 @@ const CartItems = () => {
             <hr />
             <div className='cartitems-total-item'>
               <h3>Total</h3>
-              <h3>${0}</h3>
+              <h3>${getTotalCartAmount()}</h3>
             </div>
           </div>
           <button>PROCEED TO CHECKOUT</button>
