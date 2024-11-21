80efa9bb286be6b67ab270be0b3a0768acd57af9
Alexandre Angrignon <alex040892@gmail.com>
2024-11-21 20:55:20 +1300

    responsive navbar and item section

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import './Navbar.css';
-
+import nav_dropdown from '../Assets/nav_dropdown.png';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 import { Link } from 'react-router-dom';
@@ -8,13 +8,24 @@ import { ShopContext } from '../../Context/ShopContext';
 const Navbar = () => {
   const [menu, setMenu] = useState('shop');
   const { getTotalCartItems } = useContext(ShopContext);
+  const menuRef = useRef();
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  };
   return (
     <div className='navbar'>
       <div className='nav-logo'>
         <img src={logo} alt='' />
-        <p>Shopper</p>
+        <p>SHOPPER</p>
       </div>
-      <ul className='nav-menu'>
+      <img
+        src={nav_dropdown}
+        className='nav-dropdown'
+        onClick={dropdown_toggle}
+        alt=''
+      />
+      <ul ref={menuRef} className='nav-menu'>
         <li onClick={() => setMenu('shop')}>
           <Link to='/' style={{ textDecoration: 'none' }}>
             Shop
